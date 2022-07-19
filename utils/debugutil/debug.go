d7bb3f2d33e9a6df1ff28ce75ae6f8cdd2b72c47
MisterChing <chfnaruto@gmail.com>
2022-07-19 11:05:39 +0800

    add block

--- utils/debugutil/debug.go
+++ utils/debugutil/debug.go
@@ -3,6 +3,9 @@ package debugutil
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 func DebugPrint(obj interface{}, isExit int) {
@@ -12,4 +15,29 @@ func DebugPrint(obj interface{}, isExit int) {
 	} else {
 		fmt.Printf("[debug2]---%p---%T---%+v\n", obj, obj, obj)
 	}
+}
+
+func BlockMain(fnArr ...func()) {
+	var (
+		done = make(chan struct{})
+		wg   sync.WaitGroup
+	)
+	go func() {
+		quitCh := make(chan os.Signal)
+		signal.Notify(quitCh, syscall.SIGTERM, syscall.SIGINT)
+		<-quitCh
+		if len(fnArr) > 0 {
+			for _, fn := range fnArr {
+				wg.Add(1)
+				f := fn
+				go func() {
+					defer wg.Done()
+					f()
+				}()
+			}
+			wg.Wait()
+		}
+		done <- struct{}{}
+	}()
+	<-done
 }

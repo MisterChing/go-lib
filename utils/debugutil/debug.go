6a7464b03052fb3c88bc52c2b6b402bceeff258f
MisterChing <chfnaruto@gmail.com>
2022-09-07 17:56:16 +0800

    up debug

--- utils/debugutil/debug.go
+++ utils/debugutil/debug.go
@@ -10,11 +10,19 @@ import (
 
 func DebugPrint(obj interface{}, isExit int) {
 	if isExit == 1 {
-		fmt.Printf("[debug2]---%p---%T---%+v\n", obj, obj, obj)
+		fmt.Printf("\033[1;40;32m[debug]\033[0m---%p---%T---%+v\n", obj, obj, obj)
 		os.Exit(0)
 	} else {
-		fmt.Printf("[debug2]---%p---%T---%+v\n", obj, obj, obj)
+		fmt.Printf("\033[1;40;32m[debug]\033[0m---%p---%T---%+v\n", obj, obj, obj)
 	}
+}
+
+func DebugPrintV2(module string, obj ...interface{}) {
+	fmt.Printf("\033[1;40;32m[debug]\033[0m------[%s] start\n", module)
+	for _, v := range obj {
+		fmt.Printf("\033[1;40;32m[debug]\033[0m---%p---%T---%+v\n", v, v, v)
+	}
+	fmt.Printf("\033[1;40;32m[debug]\033[0m------[%s] end\n", module)
 }
 
 func BlockMain(fnArr ...func()) {
